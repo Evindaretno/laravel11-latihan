aab6e35b76dc6e18d507126213b1b10c3907d463
Evindaretno <evindamagangjogja@gmail.com>
2025-08-20 10:43:27 +0700

    UPDATE

---
+++ app/Models/Post.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Post
+{
+    public static function all()
+    {
+        return [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Evinda Retno Jayanti',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Esse totam voluptatibus nostrum perferendis sunt! Deserunt, 
+            cum nam fuga porro aliquid obcaecati. 
+            Obcaecati et unde, velit earum veniam perferendis ea facere.',
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Evinda Retno Jayanti',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Provident et minus in? Qui voluptates, unde assumenda eum facere maxime dolores repellendus 
+            perferendis veritatis rerum atque officia necessitatibus sapiente temporibus ducimus.',
+        ]
+        ];
+    }
+
+    public static function find($slug)
+    {
+        return Arr::first(Post::all(), function ($post) use ($slug) {
+            return $post['slug'] == $slug;
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -12,55 +15,12 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Evinda Retno Jayanti',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Esse totam voluptatibus nostrum perferendis sunt! Deserunt, 
-            cum nam fuga porro aliquid obcaecati. 
-            Obcaecati et unde, velit earum veniam perferendis ea facere.',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Evinda Retno Jayanti',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Provident et minus in? Qui voluptates, unde assumenda eum facere maxime dolores repellendus 
-            perferendis veritatis rerum atque officia necessitatibus sapiente temporibus ducimus.',
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all() ]);
 });
 
 Route::get('/posts/{slug}', function($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Evinda Retno Jayanti',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Esse totam voluptatibus nostrum perferendis sunt! Deserunt, 
-            cum nam fuga porro aliquid obcaecati. 
-            Obcaecati et unde, velit earum veniam perferendis ea facere.',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Evinda Retno Jayanti',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Provident et minus in? Qui voluptates, unde assumenda eum facere maxime dolores repellendus 
-            perferendis veritatis rerum atque officia necessitatibus sapiente temporibus ducimus.',
-        ]
-        ];
 
-        $post = Arr::first($posts, function ($post) use ($slug) {
-            return $post['slug'] == $slug;
-        });
+        $post = Post::find($slug);
         
 Route::get('/post', function () {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
