34f830aae2547e53fd18b65c9a48509f04c6a9d4
Evindaretno <evindamagangjogja@gmail.com>
2025-08-23 11:33:19 +0700

    UPDATE

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,43 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Evinda Retno Jayanti',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Esse totam voluptatibus nostrum perferendis sunt! Deserunt, 
-            cum nam fuga porro aliquid obcaecati. 
-            Obcaecati et unde, velit earum veniam perferendis ea facere.',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Evinda Retno Jayanti',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Provident et minus in? Qui voluptates, unde assumenda eum facere maxime dolores repellendus 
-            perferendis veritatis rerum atque officia necessitatibus sapiente temporibus ducimus.',
-        ]
-        ];
-    }
-
-    public static function find($slug)
-    {
-        return Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $table = 'blog_posts';
 }
